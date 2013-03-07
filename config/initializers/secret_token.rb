f6b9d4fd90ed09714c24f6a9238b0a0526c1791b
Bill <roundedvision@gmail.com>
2013-03-07 13:17:56 -0800

    created project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialPlaylist::Application.config.secret_token = 'ca8d0462b650ae0b9058d9f436a302a58af4a0fcdafca91b8a6a9a2cfd0277cdeed1ab058ea2ca03c68b3de90700969d3a57a255475a0336c9570469120b6718'

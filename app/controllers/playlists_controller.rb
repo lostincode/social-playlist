79ebff0c118972699348c77674f88459ca9f9fe2
Mike Strand <mxstrand@hotmail.com>
2013-03-08 14:54:25 -0800

    created show and create actions on playlist controller and associated html files

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,7 +1,23 @@
 class PlaylistsController < ApplicationController
+
   def index
   end
+
   def new
     @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(params[:playlist])
+    if @playlist.save
+      flash[:notice] = "Your playlist has been created."
+      redirect_to @playlist
+    else
+      # nothing yet
+    end
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
   end
 end
--- spec/integration/creating_playlists_spec.rb
+++ spec/integration/creating_playlists_spec.rb
@@ -4,10 +4,10 @@ feature 'Creating Playlist' do
   scenario "can create a playlist" do
     visit '/'
     click_link 'New Playlist'
-    fill_in 'Name', :with => 'Test Playlist'
-    fill_in 'Description', :with => 'test playlist description'
-    choose('public')
+    fill_in 'playlist_name', :with => 'Test Playlist'
+    fill_in 'playlist_description', :with => 'test playlist description'
+    # choose('public')
     click_button 'Create Playlist'
-    page.should have_content "Playlist has been created."
+    page.should have_content "Your playlist has been created."
   end
 end

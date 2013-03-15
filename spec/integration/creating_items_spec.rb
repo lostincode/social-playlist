a4c30553f7fb5899f4f37cd556b134a69723f158
Bill <roundedvision@gmail.com>
2013-03-15 08:51:49 -0700

    Added test to check if URL is valid

--- spec/integration/creating_items_spec.rb
+++ spec/integration/creating_items_spec.rb
@@ -16,10 +16,19 @@ feature "Creating items" do
     page.should have_content("Item has been created.")
   end
 
-  scenario "Creating a item without valid attributes fails" do
+  scenario "Creating a item without filling in fields fails" do
     click_button "Create Item"
     page.should have_content("Item has not been created.")
     page.should have_content("Title can't be blank")
     page.should have_content("Song url is invalid")
   end
+
+  scenario "Creating a item without valid URL fails" do
+    fill_in "Title", :with => "Testing invalid URL"
+    fill_in "Url", :with => "ftp://somecrazyurl.com"
+    click_button "Create Item"
+    page.should have_content("Item has not been created.")
+    page.should have_content("Song url is invalid")
+  end
+
 end

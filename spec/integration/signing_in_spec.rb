da1024aa5616add41aaca04d9904c0d0329aa0ef
Bill <roundedvision@gmail.com>
2013-03-16 14:31:31 -0700

    adding singing in spec

---
+++ spec/integration/signing_in_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'Signing in' do
+  before do
+    Factory(:user, :email => 'user@example.com')
+  end
+
+  scenario 'Signing in via confirmation' do
+    open_email "ticketee@example.com", :with_subject => /Confirmation/
+    click_first_link_in_email
+    page.should have_content("Your account was successfully confirmed")
+    page.should have_content("Signed in as user@example.com")
+  end
+
+  scenario 'Signing in via form' do
+    User.find_by_email('user@example.com').confirm!
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', :with => "user@example.com"
+    fill_in 'Password', :with => "password"
+    click_button 'Sign in'
+    page.should have_content("Signed in successfully.")
+  end
+end
---
+++ spec/support/factories/user_factory.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) { |n| "user#{n}@ticketee.com"}
+    password "password"
+    password_confirmation "password"
+
+    factory :confirmed_user do
+      after_create do |user|
+        user.confirm!
+      end
+    end
+
+    factory :admin_user do
+      after_create do |user|
+        user.confirm!
+        user.update_attribute(:admin, true)
+      end
+    end
+
+  end
+end

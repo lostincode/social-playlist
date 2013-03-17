4034d3b310e3b4c6cea31f14c27fdfb0331a5f6d
Bill <roundedvision@gmail.com>
2013-03-16 17:46:33 -0700

    fixed viewing items spec

--- spec/integration/viewing_items_spec.rb
+++ spec/integration/viewing_items_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 feature "Viewing items" do
+  let!(:user) {Factory(:confirmed_user) }
+  let!(:user2) {Factory(:confirmed_user) }
+
   before do
-    sample_playlist = Factory(:playlist, :name => "Mike's Seattle Jams")
+    sample_playlist = Factory(:playlist, :name => "Mike's Seattle Jams", :user => user)
     sample_song = Factory(:song)
     Factory(:item, :playlist => sample_playlist, :title => "Sample track 1", :song => sample_song)
 
-    sample_playlist_2 = Factory(:playlist, :name => "Bill's Florida Summer Jams")
+    sample_playlist_2 = Factory(:playlist, :name => "Bill's Florida Summer Jams", :user => user2)
     Factory(:item, :playlist => sample_playlist_2, :title => "Sample track 2", :song => sample_song)
 
     visit '/'

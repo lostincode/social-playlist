afdaaeb900d946c50e47ddc83e0ed314f32ba0c2
Mike Strand <mxstrand@hotmail.com>
2013-03-16 17:34:21 -0700

    fixed deleting items spec

--- spec/integration/deleting_playlists_spec.rb
+++ spec/integration/deleting_playlists_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 feature "Deleting playlists" do
+  let!(:user) {Factory(:confirmed_user) }
+  let!(:playlist) {Factory(:playlist, :name => "Mike's Seattle Jams") }
+
+  before do
+    sign_in_as!(user)
+    playlist.update_attribute(:user, user)
+  end
 
   scenario "Deleting a playlist" do
-    Factory(:playlist, :name => "Test Playlist")
     visit '/'
-    click_link "Test Playlist"
+    click_link "Mike's Seattle Jams"
     click_link "Delete Playlist"
     page.should have_content("Playlist has been deleted.")
 

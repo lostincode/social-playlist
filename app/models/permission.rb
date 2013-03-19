3b2a1fcee4684742825588cf3795b2605ac354a8
Bill <roundedvision@gmail.com>
2013-03-19 10:05:42 -0700

    updated rails ver, updated permissions to block unauthorized users to contribute to private playlists

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,4 +1,5 @@
 class Permission < ActiveRecord::Base
   attr_accessible :user, :action, :thing
-  belongs_to :user, :polymorphic => true
+  belongs_to :user
+  belongs_to :thing, :polymorphic => true
 end
--- spec/integration/creating_items_spec.rb
+++ spec/integration/creating_items_spec.rb
@@ -3,14 +3,15 @@ require 'spec_helper'
 feature "Creating items" do
   let!(:playlist_owner_user) { Factory(:confirmed_user) }
   let!(:no_permission_user) { Factory(:confirmed_user) }
+  let!(:has_permission_user) { Factory(:confirmed_user) }
   let!(:playlist) { Factory(:playlist, :name => "Bills Summer Jams", :user => playlist_owner_user) }
 
+  context "Playlist owner" do
     before do
       sign_in_as!(playlist_owner_user)
       visit '/'
       click_link playlist.name
       click_link "New Item"
-      click_button "Create Item"
     end
     scenario "Creating a item" do
       fill_in "Title", :with => "Test song title"
@@ -33,5 +34,36 @@ feature "Creating items" do
       page.should have_content("Item has not been created.")
       page.should have_content("Song url is invalid")
     end
+  end
+
+  context "Unauthorized user" do
+    before do
+      sign_in_as!(no_permission_user)
+      visit '/'
+      click_link playlist.name
+      click_link "New Item"
+    end
+
+    scenario "Creating an item with a user who has permission" do
+      page.should have_content("You cannot create items on this playlist.")
+    end
+  end
+
+  context "Authorized user" do
+    before do
+      sign_in_as!(has_permission_user)
+      define_permission!(has_permission_user, "create items", playlist)
+      visit '/'
+      click_link playlist.name
+      click_link "New Item"
+    end
+
+    scenario "Creating an item with a user who has permission" do
+      fill_in "Title", :with => "Test song title"
+      fill_in "Url", :with => "http://www.youtube.com/watch?v=lZD4ezDbbu4"
+      click_button "Create Item"
+      page.should have_content("Item has been created.")
+    end
+  end
 
 end

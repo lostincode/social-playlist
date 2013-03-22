d88d232ccd58471a7e19ecd3f979f95af9d432bb
Mike Strand <mxstrand@hotmail.com>
2013-03-22 14:55:47 -0700

    Added seeds.rb data with 2 users, 3 playlist of 3 vids each.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,105 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin_user = User.create({:email => "mike@mike.com",
-                          :password => "mike"})
 
-playlist = Playlist.create({ name: "Mike's Winter Jams" },
-                            { name: "Mike's Smooth Jazz" })
+#CREATE USERS
+user = User.create( email: "mike@mike.com",
+                      password: "mike" )
+user.confirm!
 
-admin_user = User.create({:email => "bill@bill.com",
-                          :password => "bill"})
+user = User.create( email: "bill@bill.com",
+                      password: "bill" )
+user.confirm!
 
-playlist = Playlist.create({ name: "Bill's Summer Beats" },
-                            { name: "Bill's Romantic Relaxer Tunes" })
+
+#CREATE PLAYLISTS
+playlist = Playlist.create( {name: "Rustbelt Hip Hop",
+                             description:  "A collection of songs from
+                             hip hop artists in America's rust belt (Detroit, Pittsburgh, etc)",
+                             shared: true,
+                             user_id: 1}, :without_protection => true)
+
+playlist = Playlist.create( {name: "Shoegaze Synthpop with Vocals",
+                             description:  "Trance roots with a hook",
+                             shared: true,
+                             user_id: 1}, :without_protection => true)
+
+playlist = Playlist.create( {name: "*LIVE* Big Bangin' Power Ballads *LIVE*",
+                             description:  "Concert versions of big, bangin' ballads",
+                             shared: true,
+                             user_id: 2}, :without_protection => true)
+
+#CREATE SONGS
+song = Song.create( url: "http://www.youtube.com/watch?v=dcUGcBFeU_g")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=X0aXbLF_wdk")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=rv66XaNpbLo")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=dX3k_QDnzHE")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=NSf-pYdUDn0")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=Zr_MJAOyOeU")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=4u3StTsVDD0")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=jSsenVv7rF0")
+
+song = Song.create( url: "http://www.youtube.com/watch?v=X_Ur7koy6d0")
+
+
+#CREATE ITEMS
+item = Item.create( {song_id: 1,
+                     playlist_id: 1,
+                     order: 1,
+                     title: "Step to the Rear by Formula 412"},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 2,
+                     playlist_id: 1,
+                     order: 2,
+                     title: "Just Alright By Skrapz"},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 3,
+                     playlist_id: 1,
+                     order: 3,
+                     title: "Cincinnati Cypher"},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 4,
+                     playlist_id: 2,
+                     order: 1,
+                     title: "Midnight City by M83"},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 5,
+                     playlist_id: 2,
+                     order: 2,
+                     title: "The Night by School of Seven Bells (live)"},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 6,
+                     playlist_id: 2,
+                     order: 3,
+                     title: "Sigur Ros - Glosoli "},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 7,
+                     playlist_id: 3,
+                     order: 1,
+                     title: "Love Walks In by Van Halen"},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 8,
+                     playlist_id: 3,
+                     order: 2,
+                     title: "When Love and Hate Collide - Taylor Swift/Def Leppard"},
+                     :without_protection => true )
+
+item = Item.create( {song_id: 9,
+                     playlist_id: 3,
+                     order: 3,
+                     title: "What It Takes by Aerosmith"},
+                     :without_protection => true )

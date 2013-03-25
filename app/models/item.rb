b5c2a5b86438fb09c456f79e93d66e87ddde30a9
Bill <roundedvision@gmail.com>
2013-03-25 09:42:51 -0700

    moved items html to partial, added check in item model to make sure only 3 items per contributer can be added.

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,20 @@ class Item < ActiveRecord::Base
   accepts_nested_attributes_for :song
 
   before_validation :check_for_existing_song
+  validate :check_item_count_for_user
 
   def check_for_existing_song
     self.song = Song.find_or_create_by_url(song.url)
+  end
+
+  def check_item_count_for_user
+    #binding.pry
+    if self.user.id != self.playlist.user_id
+      user_item_count = self.playlist.items.find_all_by_user_id(self.user_id).count
+      if user_item_count > 3
+        errors.add(:item, 'You can only add 3 items per playlist')
+      end
+    end
   end
 
 end

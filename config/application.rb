e84fbfa6ef4404e47dd73ee2d82c9672cf00f16c
Mike Strand <mxstrand@hotmail.com>
2013-08-07 22:17:18 -0700

    application.yml load is environment dependent

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-ENV.update YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+if Rails.env == "development"
+    ENV.update YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+end
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line

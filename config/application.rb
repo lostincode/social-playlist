40e84d1c2e10f07477d44e19783c54dfab566156
Mike Strand <mxstrand@hotmail.com>
2013-08-07 22:05:57 -0700

    corrections to application.yml

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+ENV.update YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
